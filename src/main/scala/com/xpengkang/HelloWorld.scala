3a59da7d61f763af47961210f80ed231467522e9
Peng Kang <kang.peng@shopee.com>
2022-08-18 11:27:51 +0800

    hello scala

---
+++ src/main/scala/com/xpengkang/HelloWorld.scala
@@ -0,0 +1,14 @@
+package com.xpengkang
+
+/**
+  * @Author: kang.peng
+  * @Date: 2022/8/18 11:13 AM
+  * @Version 1.0
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("hello xpengkang")
+    println("every + - * / is the method in scala: ", 5.+(2222))
+  }
+
+}
